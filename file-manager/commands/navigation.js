76a03d3cf76d06e204f1d38ceb3dc1fd01f7cc7f
anturchin <tur4intwo@gmail.com>
2024-10-13 14:58:58 +0300

    refactor: remove logger in func UP

--- file-manager/commands/navigation.js
+++ file-manager/commands/navigation.js
@@ -1,16 +1,11 @@
 import { cwd, chdir } from 'node:process';
 import { dirname } from 'node:path';
 
-import { logger } from '../utils/logger.js';
-import { messages } from '../constants/index.js';
-import { getCurrentlyPath } from '../utils/helpers.js';
-
 export const up = () => {
     const currentDir = cwd();
     const parentDir = dirname(currentDir);
 
     if (parentDir !== currentDir) {
         chdir(parentDir);
-        logger.logInfo(messages.currently(getCurrentlyPath()));
     }
 };
