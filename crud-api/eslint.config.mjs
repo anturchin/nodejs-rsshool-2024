7e24e35e706a8eb59105f92388065f30b2eb51ec
anturchin <tur4intwo@gmail.com>
2024-10-20 20:28:39 +0300

    feat: update eslint config

--- crud-api/eslint.config.mjs
+++ crud-api/eslint.config.mjs
@@ -5,7 +5,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
     {
-        ignores: ['dist/*'],
+        ignores: ['dist/*', 'tests/*'],
     },
     {
         files: ['**/*.{js,ts}'],
